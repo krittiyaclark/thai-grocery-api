c708f2085dd98e70c33d13e768bc42f6c4160365
Krittiya <krittiyaclark@gmail.com>
2021-07-21 09:18:45 -0400

    Writes a basic API

--- dist/server.dev.js
+++ dist/server.dev.js
@@ -6,13 +6,34 @@ var app = express();
 
 var cors = require('cors');
 
-var PORT = 8000; // Response to locallhost 8000
+var PORT = 8000;
+app.use(cors());
+var thaiGroceries = {
+  'Thai coconut milk, Chaokoh brand': {
+    brand: 'Chaokoh',
+    info: 'PREMIUM QUALITY COCONUTS - Our creamy coconut milk is extracted from the pulp of mature coconuts which are ethically sourced from sustainable growers and have passed strict quality control inspections to ensure high quality.',
+    ingredients: 'coconut extract, water, citric acid (as antioxidant), sodium metabisulfite (as perservative). Unsweetened.',
+    size: '13.5 oz',
+    weight: '13.5 Ounces',
+    price: '3.50'
+  },
+  'Maesri Green Curry Paste': {
+    brand: 'Maesri',
+    info: 'Maesri Brand is recognized in Thailand as a high quality export product with rich taste and authentic flavor.',
+    ingredients: 'Fresh Chilli Pepper, Garlic, Chinese Ginger, Shallot, Lemongrass, Salt, Sweet Basil, Kaffir Lime, Sugar, Spices (Coriander Seeds, Pepper, Cumin, Cardamom, Turmeric), Galangal, Fresh Coriander.',
+    size: '14 oz',
+    weight: '7.7 oz',
+    price: '3.99'
+  }
+}; // Response to locallhost 8000
 
 app.get('/', function (request, response) {
   response.sendFile(__dirname + '/index.html');
 }); // Response to the other route
 
-app.get('/api/thai-grocery-product', function (request, response) {});
+app.get('/api/thai-grocery-product', function (request, response) {
+  response.json(thaiGroceries);
+});
 app.listen(PORT, function () {
   console.log("Server is running ".concat(PORT));
 }); // app.get('/api/thai-grocery-marketplace', (request, response) => {
